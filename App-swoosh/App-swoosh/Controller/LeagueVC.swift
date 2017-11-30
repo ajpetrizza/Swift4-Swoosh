0e5b1435d8b70a251ed0b12c2a802c0131aca5e0
ajpetrizza <ajpetrizza@fullsail.edu>
2017-11-30 18:47:37 -0500

    Added struct for player and button functionality

--- App-swoosh/App-swoosh/Controller/LeagueVC.swift
+++ App-swoosh/App-swoosh/Controller/LeagueVC.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     
     
@@ -27,7 +32,23 @@ class LeagueVC: UIViewController {
     }
     */
     
+    // IBActions :
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
     
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
+    
+    //Programatic segue
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
---
+++ App-swoosh/App-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  App-swoosh
+//
+//  Created by Anthony Petrizza on 11/30/17.
+//  Copyright © 2017 Anthony Petrizza. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
