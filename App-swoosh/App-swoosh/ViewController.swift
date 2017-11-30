9328a81fac19e3ea79f6d07b5902cc7f0597496a
ajpetrizza <ajpetrizza@fullsail.edu>
2017-11-30 17:48:36 -0500

    Started segues and going back

--- App-swoosh/App-swoosh/ViewController.swift
+++ App-swoosh/App-swoosh/ViewController.swift
@@ -24,11 +24,10 @@ class ViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func unwindFromLeagueVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
     
 
 }
